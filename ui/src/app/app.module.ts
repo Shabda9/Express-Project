8c5de60fe67c954b6c100105feba6d651bba9a41
Shabda <mshabda2@gmail.com>
2023-12-04 16:17:29 +0545

    feat: Integrate angular in the application - two component added for book list and book detail

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
